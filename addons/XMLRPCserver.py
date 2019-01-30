2e15b50c7a09516aa9f6716520df1cbe59d30d96
Kristen Haave <KristenH@production.local>
2019-01-30 14:16:01 +0100

    A new toolbar is implemented, but the remaining process still runs

--- addons/XMLRPCserver.py
+++ addons/XMLRPCserver.py
@@ -1,6 +1,7 @@
 import bpy, sys, os, time, socket, threading
 from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
+
 bl_info = {
     "name": "XMLRPC Server",
     "author": "KristenH",
@@ -8,6 +9,9 @@ bl_info = {
     "blender": (2, 79, 0),
     "category": "System"
 }
+
+from bpy.props import (BoolProperty, PointerProperty)
+from bpy.types import (Panel, Operator, PropertyGroup)
 
 class SimpleServer(SimpleXMLRPCServer):
     pass
@@ -35,6 +39,14 @@ def pscan(host='localhost', port=8000):
     except:
         pass
 
+# Add a turn on/off button for Blender
+class MySettings(PropertyGroup):
+    my_bool = BoolProperty(
+        name="End is checked",
+        description="End Server",
+        default = True
+    )
+
 class ServerThread(threading.Thread):
     def __init__(self, host='localhost', port=8000):
         self.proxy = (host, port)
@@ -57,25 +69,14 @@ class ServerThread(threading.Thread):
 
         except KeyboardInterrupt:
             print("Exiting")
+            self.server_thread.setDaemon(False)
             sys.exit()
 
-class ServerPanel(bpy.types.Panel):
-    bl_label = "Server Panel"
-    bl_idname = "Object_PT_server"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_category = "Shortcuts"
-    #server = ServerThread()
-
-    def draw(self, context):
-        # server = run_server()
-        layout = self.layout
-        layout.row().label(text="Server found on: {}:{}".format(server.host, server.port))
-
-
+global server
 server = None
-x=8000
-while(server is None):
+global x
+x = 8000
+while (server is None):
     con = pscan(port=x)
     if con is not None:
         print("Server found on: localhost:", x)
@@ -84,13 +85,71 @@ while(server is None):
         server = ServerThread(port=x)
         print("Started the server with:", server.host, ":", server.port)
         server.start()
-        print("... Press Ctrl+C to exit")
+
+class ServerOperator(bpy.types.Operator):
+    bl_idname = "wm.xmlrpc_server"
+    bl_label = "Terminate"
+
+    def execute(self, context):
+        scene = context.scene
+        mytool = scene.my_tool
+        global server
+        global x
+        #layout = self.layout
+        if (mytool.my_bool == True):
+            while (server is not None):
+                con = pscan(port=x)
+                server = ServerThread(port=x)
+                print("Server with", server.host, ":", server.port, "terminated.")
+                server.exit()
+                x -= 1
+        else:
+            pass
+        return {'FINISHED'}
+
+class ServerMenu(bpy.types.Menu):
+    bl_idname = "OBJECT_MT_select_test"
+    bl_label = "Select"
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.operator("object.select_all", text="Select/Deselect ALL").action = 'TOOGLE'
+        layout.operator("object.select_all", text="Inverse").action = 'INVERT'
+        layout.operator("object.select_random", text="Random")
+
+class ServerPanel(Panel):
+    bl_label = "Server"
+    bl_idname = "Object_PT_server"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    bl_category = "Tools"
+    bl_context = "objectmode"
+    #server = ServerThread()
+
+    @classmethod
+    def poll(self, context):
+        return context.object is not None
+
+    def draw(self, context):
+        # server = run_server()
+        layout = self.layout
+        scene = context.scene
+        mytool = scene.my_tool
+
+        layout.prop(mytool, "my_bool")
+        layout.operator("wm.xmlrpc_server")
+
 
 def register():
-    bpy.utils.register_class(ServerPanel)
+    bpy.utils.register_module(__name__)
+    bpy.types.Scene.my_tool = PointerProperty(type=MySettings)
+    #bpy.utils.register_class(ServerPanel)
 
 def unregister():
-    bpy.utils.unregister_class(ServerPanel)
+    bpy.utils.unregister_module(__name__)
+    del bpy.types.Scene.my_tool
+    #bpy.utils.unregister_class(ServerPanel)
 
 if __name__ == "__main__":
     register()
