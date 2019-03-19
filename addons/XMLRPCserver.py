06daf40c53bf2d6da9c4951c1e18fd495c90abb6
PRODUCTION\HalimN <hal@futureuniverse.com>
2019-03-19 18:39:45 +0100

    seems good to go to the community

--- addons/XMLRPCserver.py
+++ addons/XMLRPCserver.py
@@ -1,13 +1,13 @@
-import bpy, socket, threading, platform, subprocess, os
+import bpy, socket, threading, os
 from xmlrpc.server import SimpleXMLRPCServer
 
 # basic infos
 bl_info = {
-    "name": "XMLRPC Server",
-    "author": "KristenH",
-    "version": (0, 5, 0, 1),
-    "blender": (2, 80, 0),
-    "category": "System"
+    'name': 'XMLRPC Server',
+    'author': 'Kristen Haave | Halim Negadi',
+    'version': (0, 5, 0, 1),
+    'blender': (2, 80, 0),
+    'category': 'System'
 }
 # where are we running from ?
 print(os.path.dirname(os.path.abspath(__file__)))
@@ -34,7 +34,6 @@ def command(com):
 def server_data():
     return {
         'hostname':socket.gethostname(),
-        'port':int(bpy.props.xmlrpc_port[1]['name']),
         'app_version':bpy.app.version_string,
         'file_path':bpy.context.blend_data.filepath.replace('\\', '/'),
         'exe':os.path.basename(bpy.app.binary_path),
@@ -42,38 +41,37 @@ def server_data():
         'pid':os.getpid()
         }
 
-# Server class
+# ServerThread class
 class ServerThread(threading.Thread):
-    '''A thread class that supports raising exception in the thread from
-       another thread.'''
     def __init__(self, host='localhost', port=8000):
         self._stop_event = threading.Event()
         self.proxy = (host, port)
         self.server = SimpleXMLRPCServer(self.proxy)
         self.server.register_introspection_functions()
-        self.server.register_function(command, "command")
-        self.server.register_function(server_data, "server_data")
+        self.server.register_function(command, 'command')
+        self.server.register_function(server_data, 'server_data')
         self.thread = threading.Thread(target=self.server.serve_forever)
         self.port = port
         self.host = host
+        self.hostname = socket.gethostname()
 
+    # we make sure to demonize so there's no zombie remain after quitting blender
     def start(self):
         self.thread.setDaemon(True)
         self.thread.start()
 
 
 class ServerPanel(bpy.types.Panel):
-    bl_label = "Server Panel"
-    bl_idname = "Object_PT_server"
+    bl_label = 'Server Panel'
+    bl_idname = 'Object_PT_server'
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
-    bl_category = "Shortcuts"
-    bl_context = "world"
+    bl_category = 'Shortcuts'
+    bl_context = 'world'
 
     def draw(self, context):
-        # server = run_server()
         layout = self.layout
-        layout.row().label(text="Server running on: {}:{}".format(server.host, server.port))
+        layout.row().label(text='Server running on {}:{}'.format(server.hostname, server.port))
 
 
 server = None
@@ -81,12 +79,11 @@ x=8000
 while(server is None):
     con = pscan(port=x)
     if con is not None:
-        print("Server found on: localhost:", x)
+        print('Server found on: localhost:', x)
         x += 1
     else:
         server = ServerThread(port=x)
         server.start()
-        bpy.props.xmlrpc_port = bpy.props.BoolProperty(name=x)
 
 
 
@@ -101,5 +98,5 @@ def unregister():
     from bpy.utils import unregister_class
     unregister_class(ServerPanel)
        
-if __name__ == "__main__":
+if __name__ == '__main__':
     register()
